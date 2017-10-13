b0968ddc87dfcfb4251cb3a2530256000f188d17
rvogel <vogel@hallowelt.biz>
2017-10-13 16:31:28 +0200

    Basic implementation for domain/username/password authentification

---
+++ src/Auth/DomainAndPasswordAuthenticationRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace MediaWiki\Extension\LDAPAuthentication\Auth;
+
+use MediaWiki\Auth\PasswordAuthenticationRequest;
+use MediaWiki\Extension\LDAPProvider\ClientFactory;
+
+class DomainAndPasswordAuthenticationRequest extends PasswordAuthenticationRequest {
+
+	public $domain = '';
+
+	public function getFieldInfo(): array {
+		$ret = parent::getFieldInfo();
+
+		$ret ['domain'] = [
+			'type' => 'select',
+			'options' => $this->makeDomainOptions(),
+			'label' => wfMessage( 'ldapauthentication-label-domain' ),
+			'help' => wfMessage( 'ldapauthentication-help-domain' ),
+		];
+
+		return $ret;
+	}
+
+	protected function makeDomainOptions() {
+		$domainOpts = [
+			'local' => wfMessage( 'ldapauthentication-domain-local' ),
+		];
+
+		$clientFactory = ClientFactory::getInstance();
+		$configuredDomains = $clientFactory->getConfiguredDomains();
+		foreach( $configuredDomains as $configuredDomain ) {
+			$domainOpts[ $configuredDomain ] =
+				wfMessage( 'ldapauthentication-domain-'.$configuredDomain );
+		}
+
+		return $domainOpts;
+	}
+
+}
--- src/Setup.php
+++ src/Setup.php
@@ -4,7 +4,10 @@ namespace MediaWiki\Extension\LDAPAuthentication;
 
 class Setup {
 	public static function onRegistration() {
-
+		/*$GLOBALS['wgSessionProviders']['MyCustomSessionProvider'] = [
+			'class' => 'MyCustomSessionProvider',
+			'args' => [ 'arg1', 'arg2' ],
+		];*/
 	}
 }
 
