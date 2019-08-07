1bf50ed22bcc51c81d3f852533938a924cc948aa
rvogel <vogel@hallowelt.biz>
2019-08-07 07:50:15 +0200

    Add UnitTest for class `Setup`
    Change-Id: I598f7eda4756dca46e7af851ddd30784ffe8f736

---
+++ tests/phpunit/SetupTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace MediaWiki\Extension\LDAPAuthentication2\Tests;
+
+use MediaWiki\Extension\LDAPAuthentication2\Setup;
+use MediaWiki\Extension\LDAPProvider\DomainConfigProvider\InlinePHPArray;
+use MediaWikiTestCase;
+
+/**
+ * @coversDefaultClass MediaWiki\Extension\LDAPAuthentication2\Setup
+ */
+class SetupTest extends MediaWikiTestCase {
+
+	/**
+	 * @covers MediaWiki\Extension\LDAPAuthentication2\Setup::init
+	 */
+	public function testInit() {
+		$this->setMwGlobals( [
+			'wgPluggableAuth_ExtraLoginFields' => [],
+			'LDAPProviderDomainConfigProvider' => function () {
+				$config = [ 'hw.local' => [] ];
+				return new InlinePHPArray( $config );
+			}
+		] );
+
+		Setup::init();
+
+		$this->assertArrayHasKey( 'domain', $GLOBALS['wgPluggableAuth_ExtraLoginFields'] );
+		$this->assertArrayHasKey( 'username', $GLOBALS['wgPluggableAuth_ExtraLoginFields'] );
+		$this->assertArrayHasKey( 'password', $GLOBALS['wgPluggableAuth_ExtraLoginFields'] );
+	}
+}
