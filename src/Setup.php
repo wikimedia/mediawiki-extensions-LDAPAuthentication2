0404702d4fb5f9cb609d725004c1829269514264
rvogel <vogel@hallowelt.biz>
2020-11-27 17:13:04 +0100

    Hide MediaWiki standard Login button
    ... when "LocalLogin" is enabled.
    
    Be aware, that PluggableAuth and LDAPAuthentication2 must be configured
    synchronously.
    
    NEEDS CHERRY-PICK TO `REL1_35` and `master`
    
    ERM20192
    
    Change-Id: I7b9de605316a9d89454881df2cbae1a87d356ec0

--- src/Setup.php
+++ src/Setup.php
@@ -3,6 +3,8 @@
 namespace MediaWiki\Extension\LDAPAuthentication2;
 
 use MediaWiki\Extension\LDAPProvider\DomainConfigFactory;
+use OutputPage;
+use Skin;
 
 class Setup {
 	/**
@@ -13,5 +15,26 @@ class Setup {
 		$config = Config::newInstance();
 		$GLOBALS['wgPluggableAuth_ExtraLoginFields']
 			= (array)( new ExtraLoginFields( $configuredDomains, $config ) );
+	}
+
+	/**
+	 *
+	 * @param OutputPage $out
+	 * @param Skin $skin
+	 * @return void
+	 */
+	public static function onBeforePageDisplay( $out, $skin ) {
+		/**
+		 * If `$LDAPAuthentication2AllowLocalLogin` is enabled we will end up with two "Login"
+		 * buttons, as `PluggableAuth` will leave the regular
+		 * `LocalPasswordPrimaryAuthenticationProvider` enabled. As there is unfortunately no
+		 * other way to remove it - besides implementing subclass of
+		 * `LocalPasswordPrimaryAuthenticationProvider`, we just hide it
+		 */
+		$config = new Config();
+		if ( $out->getTitle()->isSpecial( 'Userlogin' ) && $config->get( 'AllowLocalLogin' ) ) {
+			$out->addInlineStyle( '#wpLoginAttempt { display: none; }' );
+		}
+		return true;
 	}
 }
