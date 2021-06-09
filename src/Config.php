1d9593d27af9ca4d293c9ef4bc6a9ee892f08987
Oliver Welter <oliver@welter.rocks>
2021-06-09 12:55:09 +0200

    Added support for SSL client certificates with LDAP boundary

--- src/Config.php
+++ src/Config.php
@@ -27,4 +27,108 @@ class Config extends GlobalVarConfig {
 	public static function getVersion() {
 		return self::VERSION;
 	}
+
+	/**
+	 * Helper function to retrieve SSL config as object
+	 *
+	 * @return stdClass
+	 */
+        public static function getSSLConfig()
+        {
+                $sslconfig = new \stdClass;
+
+                $sslconfig->enabled = $GLOBALS["LDAPAuthentication2SSLClientCertEnabled"];
+                $sslconfig->prefix = $GLOBALS["LDAPAuthentication2SSLClientCertPrefix"];
+                $sslconfig->field = $GLOBALS["LDAPAuthentication2SSLClientCertField"];
+                $sslconfig->fieldParse = $GLOBALS["LDAPAuthentication2SSLClientCertFieldParse"];
+                $sslconfig->fieldSeparator = $GLOBALS["LDAPAuthentication2SSLClientCertFieldSeparator"];
+                $sslconfig->keyValueSeparator = $GLOBALS["LDAPAuthentication2SSLClientCertKeyValueSeparator"];
+                $sslconfig->keySelector = $GLOBALS["LDAPAuthentication2SSLClientCertKeySelector"];
+                $sslconfig->valueSeparators = $GLOBALS["LDAPAuthentication2SSLClientCertValueSeparators"];
+                $sslconfig->requirePassword = $GLOBALS["LDAPAuthentication2SSLClientCertRequirePassword"];
+                $sslconfig->autoLogin = $GLOBALS["LDAPAuthentication2SSLClientCertAutoLogin"];
+
+                return $sslconfig;
+        }
+        
+	/**
+	 * Helper function to retrieve SSL client cert username
+	 *
+	 * @return string
+	 */
+        public static function getSSLUsername()
+        {
+                $sslconfig = self::getSSLConfig();
+                
+                if (!$sslconfig->prefix)
+                        $sslconfig->prefix = "SSL_CLIENT_S_";
+
+                if (!$sslconfig->field)
+                        return null;
+
+                if (!$sslconfig->fieldParse)
+                {
+                        if (isset($_SERVER[$sslconfig->prefix.$sslconfig->field]))
+                                return $_SERVER[$sslconfig->prefix.$sslconfig->field];
+                        else
+                                return null;
+                }
+
+                if (!$sslconfig->keySelector)
+                        return null;
+
+                $fields = explode($sslconfig->fieldSeparator, $_SERVER[$sslconfig->prefix.$sslconfig->field]);
+
+                if (!count($fields))
+                        return null;
+
+                $username = null;
+
+                foreach ($fields as $keyvalue)
+                {
+                        if ($sslconfig->keyValueSeparator)
+                        {
+                                $keyval = explode($sslconfig->keyValueSeparator, $keyvalue);
+
+                                $key = $keyval[0];
+                                $val = ((isset($keyval[1])) ? $keyval[1] : null);
+
+                                if ($key == $sslconfig->keySelector)
+                                {
+                                        if ($sslconfig->valueSeparators)
+                                        {
+                                                if (is_array($sslconfig->valueSeparators))
+                                                {
+                                                        foreach ($sslconfig->valueSeparators as $sepkey => $sepvalue)
+                                                        {
+                                                                $sepval = explode($sepkey, $val);
+                                                                $val = ((isset($sepval[$sepvalue])) ? $sepval[$sepvalue] : $val);
+                                                        }
+                                                }
+                                                else
+                                                {
+                                                        $sepval = explode($sslconfig->valueSeparators, $val);
+                                                        $val = $sepval[0];
+                                                }
+                                        }
+
+                                        $username = $val;
+                                        break;
+                                }
+                        }
+                        else
+                        {
+                                if ($keyvalue == $sslconfig->keySelector)
+                                {
+                                        $username = $keyvalue;
+                                        break;
+                                }
+                        }
+                }
+
+                if ($username)
+                        return $username;
+
+                return null;
+        }
 }
--- src/ExtraLoginFields.php
+++ src/ExtraLoginFields.php
@@ -7,6 +7,7 @@ class ExtraLoginFields extends \ArrayObject {
 	const DOMAIN = 'domain';
 	const USERNAME = 'username';
 	const PASSWORD = 'password';
+	const SSLUSER = 'ssluser';
 
 	const DOMAIN_VALUE_LOCAL = 'local';
 
@@ -15,22 +16,73 @@ class ExtraLoginFields extends \ArrayObject {
 	 * @param Config $config Config for this extension
 	 */
 	public function __construct( array $configuredDomains, $config ) {
-		parent::__construct( [
-			static::DOMAIN => $this->makeDomainFieldDescriptor( $configuredDomains, $config ),
-			static::USERNAME => [
-				'type' => 'string',
-				'label' => wfMessage( 'userlogin-yourname' ),
-				'help' => wfMessage( 'authmanager-username-help' ),
-			],
-			static::PASSWORD => [
-				'type' => 'password',
-				'label' => wfMessage( 'userlogin-yourpassword' ),
-				'help' => wfMessage( 'authmanager-password-help' ),
-				'sensitive' => true,
-			]
-		] );
-	}
+		$sslconfig = $config->getSSLConfig();
+		$default_login = false;
 
+		if ($sslconfig->enabled)
+		{
+			if ($config::getSSLUsername())
+			{
+				if ($sslconfig->requirePassword)
+					parent::__construct( [
+						static::DOMAIN => $this->makeDomainFieldDescriptor( $configuredDomains, $config ),
+						static::SSLUSER => $this->makeSSLUserFieldDescriptor( $config ),
+						static::PASSWORD => $this->makePasswordFieldDescriptor()
+					] );
+				else
+					parent::__construct( [
+						static::DOMAIN => $this->makeDomainFieldDescriptor( $configuredDomains, $config ),
+						static::SSLUSER => $this->makeSSLUserFieldDescriptor( $config )
+					] );
+			}
+			else
+			{
+				$default_login = true;
+			}
+		}
+		else
+		{
+			$default_login = true;
+		}
+		
+		if ($default_login)
+		{
+			parent::__construct( [
+				static::DOMAIN => $this->makeDomainFieldDescriptor( $configuredDomains, $config ),
+				static::USERNAME => $this->makeUsernameFieldDescriptor(),
+				static::PASSWORD => $this->makePasswordFieldDescriptor()
+				
+			] );
+		}
+	}
+	
+	private function makeSSLUserFieldDescriptor($config)
+	{
+		return [
+			'type' => 'hidden',
+			'value' => $config::getSSLUsername()
+		];
+	}
+	
+	private function makeUsernameFieldDescriptor() 
+	{		
+		return [
+			'type' => 'string',
+			'label' => wfMessage( 'userlogin-yourname' ),
+			'help' => wfMessage( 'authmanager-username-help' ),
+		];
+	}
+	
+	private function makePasswordFieldDescriptor() 
+	{		
+		return [
+			'type' => 'password',
+			'label' => wfMessage( 'userlogin-yourpassword' ),
+			'help' => wfMessage( 'authmanager-password-help' ),
+			'sensitive' => true,
+		];
+	}
+	
 	private function makeDomainFieldDescriptor( $configuredDomains, $config ) {
 		if ( $config->get( 'AllowLocalLogin' ) ) {
 			$configuredDomains[] = static::DOMAIN_VALUE_LOCAL;
